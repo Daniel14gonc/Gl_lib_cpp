7655384a04ac7977773c36142dda8b89875a434c
Daniel14gc <gon20293@uvg.edu.gt>
2022-07-19 12:41:33 -0600

    feat: house drawn with lines

--- src/house.cpp
+++ src/house.cpp
@@ -9,11 +9,48 @@ int main()
 	glInit();
 	glCreateWindow(500, 500);
 	glViewPort(0, 0, 500, 500);
-	float y0 = -0.15;
-	float y1 = -0.45;
 
+	// Chimenea
 	glColor(0.7, 0, 0);
+	float y0 = 0.665;
+	float y1 = 0.637;
+	for(int i = 0; i < 100; i++)
+	{
+		glLine(-0.1, y0, -0.05, y1);
+		y0 += 0.001;
+		y1 += 0.001;
+	}
+
+
+	glColor(0.8, 0, 0);
+	y0 = 0.737;
+	y1 = 0.765;
+	for(int i = 0; i < 200; i++)
+	{
+		glLine(-0.05, y0, 0, y1);
+		y0 -= 0.001;
+		y1 -= 0.001;
+	}
+
+	glColor(0.9, 0, 0);
+	y0 = 0.765;
+	y1 = 0.79;
+	float x0 = -0.1;
+	float x1 = -0.05;
+	for(int i = 0; i < 230; i++)
+	{
+		glLine(x0, y0, x1, y1);
+		y0 -= 0.0001;
+		y1 -= 0.0001;
+		x0 += 0.00022;
+		x1 += 0.00022;
+	}
+
+
 	// Pared lateral
+	y0 = -0.15;
+	y1 = -0.45;
+	glColor(0.7, 0, 0);
 	for (int i = 0; i < 100; i++)
 	{
 		glLine(-0.75, y0, -0.2, y1);
@@ -22,6 +59,7 @@ int main()
 	}
 
 	//Pared frontal
+	glColor(0.8, 0, 0);
 	y0 = -0.45;
 	y1 = -0.1;
 	for (int i = 0; i < 100; i++)
@@ -32,11 +70,11 @@ int main()
 	}
 
 	//Techo lateral
-	glColor(1, 0, 0);
+	glColor(0.9, 0, 0);
 	y0 = 0.25;
 	y1 = 0.8;
-	float x0 = -0.75;
-	float x1 = -0.35;
+	x0 = -0.75;
+	x1 = -0.35;
 	for (int i = 0; i < 5450; i++)
 	{
 		glLine(x0, y0, x1, y1);
@@ -44,9 +82,27 @@ int main()
 		y0 -= 0.000055;
 		x1 += 0.0001;
 		y1 -= 0.000055;
+	}
+
+	// Techo frontal
+	glColor(0.8, 0, 0);
+	y0 = -0.05;
+	y1 = 0.50025;
+	for (int i = 0; i < 95; i++)
+	{	
+		glLine(-0.2, y0, 0.195, y1);
+		y1 -= 0.004;
 	}	
 
-	//Puerta
+	y0 = 0.50025;
+	y1 = 0.3;
+	for (int i = 0; i < 95; i++)
+	{	
+		glLine(0.6, y1, 0.195, y0);
+		y0 -= 0.004;
+	}
+
+	// Puerta
 	glColor(1, 1, 1);
 	y0 = -0.3;
 	y1 = -0.215;
@@ -56,6 +112,8 @@ int main()
 		y0 += 0.004;
 		y1 += 0.004;
 	}
+
+	
 
 	glFinish();
 	return 0;
